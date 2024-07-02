91e3ec68ddfaa1155fb87d5b1f1769fb4691806f
VinayR-GitHub <vinayraghavan7@gmail.com>
2024-07-02 19:57:21 +1000

    Changed incorrect file content and added 1 new Codeforces problem: "Vasya and Petya's Game".

--- src/Codeforces/Rating/1500/vasya_and_petyas_game.cpp
+++ src/Codeforces/Rating/1500/vasya_and_petyas_game.cpp
@@ -4,17 +4,14 @@
 #define MAX std::max
 #define MAG std::abs
 #define PAIR std::make_pair
-#define FILE(id) \
+#define ACCEL() \
 	std::ios::sync_with_stdio(NULL); \
-    std::cin.tie(NULL); \
-    freopen(#id "in.txt", "r", stdin); \
-    freopen(#id "out.txt", "w", stdout)
+    std::cin.tie(NULL);
 
 int main() {
-    FILE(prime);
     int n; std::cin >> n;
     std::vector<int> primes;
-    for (int i = 2; i < n + 1; ++i) {
+    for (int i = 2; i <= n; ++i) {
         for (auto& prime : primes) {
             if (prime > std::sqrt(i)) break;
             if (i % prime == 0) goto NEXT;
@@ -22,10 +19,17 @@ int main() {
         primes.push_back(i);
         NEXT:;
     }
-
+    std::vector<int> primepowers;
     for (auto& prime : primes) {
-        std::cout << prime << '\n';
+        int val = prime;
+        while (val <= n) {
+            primepowers.push_back(val);
+            val *= prime;
+        }
     }
-
+    std::cout << primepowers.size() << '\n';
+    for (auto& pp : primepowers) {
+        std::cout << pp << ' ';
+    }
     return 0;
 }
