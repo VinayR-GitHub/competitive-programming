76369ea61b2cce65e3e2e76cbea366bb5f4e054b
VinayR-GitHub <vinayraghavan7@gmail.com>
2024-07-10 05:07:08 +1000

    Added 1 new Codeforces problem: "Maximum Multiple Sum".

---
+++ src/Codeforces/Contests/2024-06-12/maximum_multiple_sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define MIN std::min
+#define MAX std::max
+#define MAG std::abs
+#define PAIR std::make_pair
+#define ACCEL() \
+	std::ios::sync_with_stdio(NULL); \
+    std::cin.tie(NULL);
+
+int main() {
+    ACCEL();
+    int t; std::cin >> t;
+    while (t--) {
+        int n; std::cin >> n;
+        if (n == 3) std::cout << "3\n";
+        else std::cout << "2\n";
+    }
+    return 0;
+}
